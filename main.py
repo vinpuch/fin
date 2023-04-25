a9d70a20a9db3cbb825fe5c4e9f3397687acf75a
vinpuch <94136786+vinpuch@users.noreply.github.com>
2023-04-25 19:14:33 +0200

    es kann von jedem Betreibsystem gestartet werden

--- main.py
+++ main.py
@@ -4,10 +4,19 @@ from datetime import datetime
 import os
 import logging
 from prepare import update_excel_file
+import sys
+
+if sys.platform == "win" :
+    path = "/home/vinpuch/Dokumente/IdeaProjects/finanzen/Daten"
+elif sys.platform == "darwin":
+    path = "/Users/vincentpuchner/Dokumente/IdeaProjects/fin/Daten"
+elif sys.platform == "linux":
+    path="/home/user/Dokumente/Idea Projects/fin/Daten/4_2023.xlsx"
 
 
 
-path = "/home/vinpuch/Dokumente/IdeaProjects/finanzen/Daten"
+
+
 filename = str(datetime.now().month) + '_' + str(datetime.now().year) + ".xlsx"
 
 update_excel_file(path, filename)
--- prepare.py
+++ prepare.py
@@ -10,7 +10,7 @@ def update_excel_file(path, filename):
     month_year = now.strftime("%B_%Y")
 
     db = pd.read_excel(path+"/"+ filename)
-    logging.info("Die Datei "+ filename + " wurde gefunden mit "+ month_year)
+    print("Die Datei "+ filename + " wurde gefunden mit "+ month_year)
     db["Buchnungstag"]= pd.to_datetime(db.Buchungstag)
     db.loc[(db.Buchungstag.dt.year == now.year) & (db.Buchungstag.dt.month == now.month), ["Betrag","Kontostand","Analyse-Hauptkategorie" , 
                                                                                            "Analyse-Unterkategorie"]].to_excel("./Daten/Ergebnisse/Ergebnis.xlsx")
